6c10c28f47bcc6c879612b93cf6c20c7b7df5f56
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-06-09 22:11:59 +0530

    creation form action

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -11,6 +11,13 @@ defmodule Discuss.TopicController do
   end
 
   def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 
 end
