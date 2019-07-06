66b3c7e3af149dbbdd803f79950bd53036112b0f
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-07-06 22:47:35 +0530

    upserting user

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -2,16 +2,32 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.puts "+++++"
-    IO.inspect(conn.assigns)
-    IO.puts "+++++"
-    IO.inspect(params)
-    IO.puts "+++++"
+  alias Discuss.User
+
+  def callback(%{assigns: %{ueberauth_auth: auth }} = conn, params) do
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: params["provider"]
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+
   end
 
   def request(conn, params) do
 
   end
+
+  defp upsert_user(changeset) do
+    IO.inspect(changeset)
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+
 
 end
