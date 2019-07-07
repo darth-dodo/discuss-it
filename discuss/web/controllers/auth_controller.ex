059caaefd68916373a26713f7849b76537563468
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-07-07 07:30:09 +0530

    creating set user plug

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -4,11 +4,12 @@ defmodule Discuss.AuthController do
 
   alias Discuss.User
 
+  # assigns property is used to stash data between different parts of our app
   def callback(%{assigns: %{ueberauth_auth: auth }} = conn, params) do
     user_params = %{
       token: auth.credentials.token,
       email: auth.info.email,
-      provider: params["provider"]
+      provider: get_provider(params)
     }
 
     changeset = User.changeset(%User{}, user_params)
@@ -30,6 +31,10 @@ defmodule Discuss.AuthController do
         |> redirect(to: topic_path(conn, :index))
     end
 
+  end
+
+  defp get_provider(params) do
+    params["provider"]
   end
 
   defp upsert_user(changeset) do
