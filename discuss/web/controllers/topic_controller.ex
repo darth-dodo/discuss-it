4b65a8cfd762850f691fe42ef730b48ec2763dd7
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-07-07 13:16:54 +0530

    only owners can edit or delete the topics

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,6 +1,8 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+
   plug Discuss.Plugs.RequireAuth when action in [:new, :create, :edit, :update, :delete]
+  plug :check_topic_owner when action in [:edit, :update, :delete]
 
   alias Discuss.Topic
 
@@ -70,6 +72,21 @@ defmodule Discuss.TopicController do
     conn
     |> put_flash(:info, "Topic Deleted!")
     |> redirect(to: topic_path(conn, :index))
+  end
+
+  # params data coming in from the prev plug
+  def check_topic_owner(conn, _params) do
+    %{params: %{"id" => topic_id }} = conn
+
+    if Repo.get(Topic, topic_id).user_id == conn.assigns.current_user.id do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You cannot edit that!")
+      |> redirect(to: topic_path(conn, :index))
+      |> halt()
+    end
+
   end
 
 end
