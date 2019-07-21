52cfa0133b437870f9a6f2be20f73672c8d29654
Abhishek Juneja <abhishek.juneja145@gmail.com>
2019-07-21 09:58:04 +0530

    commenting the caching static manifest in prod.ex

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :discuss, Discuss.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "NAME_OF_HEROKU_APP.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  # cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
